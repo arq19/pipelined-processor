1a530fd0ba4b4a721a65af6acb9ac577976dff5c
Mayco <maico.molina.aoki@gmail.com>
2019-11-08 12:30:28 -0300

    Add ex3 test

--- imem.sv
+++ imem.sv
@@ -6,12 +6,23 @@ module imem #(parameter N = 32)
 	
 	initial begin
 		ROM = '{default:0};
-		ROM[0:3] = '{
-				32'h91003fe1, 	// addi	x1, xzr, #15
-				32'h00000000, 	// nop
-				32'h00000000, 	// nop
-				32'hf8000001 	// stur	x1, [x0, #0]
-				};
+		ROM[0:8] = '{
+				32'h91003c0a, 	// add	x10, x0, #0xf
+				32'haa14018b, 	// orr	x11, x12, x20
+				32'h8a14018c, 	// and	x12, x12, x20
+				32'h8b0a0000, 	// add	x0, x0, x10
+				32'hcb01014a, 	// sub	x10, x10, x1
+				32'hf80003eb, 	// stur	x11, [x30, #0]
+				32'hf80083ec, 	// stur	x12, [x30, #8]
+				32'hb5ffff8a, 	// cbnz	x10, c <loop>
+				32'hf80103e0 	// stur	x0, [x30, #16]
+			};
+//		ROM[0:3] = '{
+//				32'h91003fe1, 	// addi	x1, xzr, #15
+//				32'h00000000, 	// nop
+//				32'h00000000, 	// nop
+//				32'hf8000001 	// stur	x1, [x0, #0]
+//				};
 		
 //		ROM[0:13] = '{
 //				32'h8b0603e0, 	// add	x0, xzr, x6
